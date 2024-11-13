8b33438d69238dc8369a68108baeeae86d3130b7
Jan Friebe <friebe@odas.biz>
2024-11-13 19:35:24 +0100

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var resumeData = require('./resume.json')
 
 Handlebars.registerHelper('breaklines', function (text) {
     text = Handlebars.Utils.escapeExpression(text);
-    text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+    text = text.replace(/(\r\n|\n|\r)/gm, ' <br> ');
     return new Handlebars.SafeString(text);
 })
 
